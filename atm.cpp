a87ef49dde6f26c03c34f2f731830ae399128405
mak <makindosx@gmail.com>
2017-09-18 02:30:42 +0300

    Update atm.cpp

--- atm.cpp
+++ atm.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2015 Barchampas Gerasimos </makindosx@gmail.com>
- * pseudo-atm is a programm which helps to understand atm .
+ * pseudo-atm is a programm which helps to understand atm.
  *
  * pseudo-atm is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
